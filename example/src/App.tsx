cd5261d7f11d0426bb6863bda825a753da374996
Joe Julik <jjulik@outlook.com>
2022-04-08 21:21:38 -0500

    Add react-native-maps, update @types/react to 18.0.0 to match react-native type definitions

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,22 @@
-import * as React from 'react';
+import React, { useState } from 'react';
+import MapView, { Region } from 'react-native-maps';
 
 import { StyleSheet, View } from 'react-native';
 import { MapsTileOverlayView } from 'react-native-maps-tile-overlay';
 
 export default function App() {
+  const [initialLocation] = useState<Region>({
+    latitude: 39.833333,
+    longitude: -98.583333,
+    latitudeDelta: 0.3,
+    longitudeDelta: 0,
+  });
+
   return (
     <View style={styles.container}>
-      <MapsTileOverlayView color="#32a852" style={styles.box} />
+      <MapView style={styles.map} initialRegion={initialLocation}>
+        <MapsTileOverlayView color="#32a852" style={styles.box} />
+      </MapView>
     </View>
   );
 }
@@ -21,5 +31,9 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
